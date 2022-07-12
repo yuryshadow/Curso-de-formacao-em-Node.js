61067aee9cae04abad887547e71ee7aecd6cdde5
Yuri Carlos <yuryshadow@gmail.com>
2022-07-12 15:22:33 -0300

    Introducao ao nodejs

---
+++ 1. Introdução ao Node.JS/Inicio/app.js
@@ -0,0 +1,25 @@
+var calculadora = require("./calculadora");
+
+calculadora.nome = "Calculadora do Yuri";
+console.log(calculadora.nome);
+console.log(calculadora.mult(10,20));
+console.log(calculadora.soma(20,40));
+
+
+
+
+
+
+
+/* -- Seu primeiro código em Node.js
+
+var mostrarSite = false;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello World!");
+console.log("Meu nome é Yuri!");
+console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+if(mostrarSite){ // Se mostrar site
+    console.log(site);  // Printe o site
+}*/
