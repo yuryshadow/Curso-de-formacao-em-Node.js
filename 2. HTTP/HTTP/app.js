f0275f5b53ee88e8db7043511d9373ff76da05d9
Yuri Carlos <yuryshadow@gmail.com>
2022-07-13 11:28:05 -0300

    HTTP

---
+++ 2. HTTP/HTTP/app.js
@@ -0,0 +1,6 @@
+var http = require("http");
+
+http.createServer(function(requisicao,resposta){
+    resposta.end("<h1>Bem vindo ao meu site!</h1><br><h4>guiadoprogramador.com</h4>");
+}).listen(3000);
+console.log("Meu servidor está rodando!");
