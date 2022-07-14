dcf23c0b6ddda997a08afa063b1d81f589f43891
Yuri Carlos <yuryshadow@gmail.com>
2022-07-14 16:36:03 -0300

    MYSQL Basico

---
+++ 4. MySQL Basico/comandos.sql
@@ -0,0 +1,16 @@
+-- Criar uma tabela
+
+CREATE TABLE usuarios(
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    idade INT
+);
+
+-- Inserindo dados na tabela
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Marry",
+    "email@teste00.com",
+    3
+);
+
